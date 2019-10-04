70e45260beaffe44b0aa802dfbe15b79ec3296d2
Huy Tran <huy@loopcommerce.com>
2019-10-04 13:35:27 -0700

    Initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export const App = () => {
+  return <div className="w-full h-full flex flex-col">
+    <div className="flex-1 p-5">
+      Hello World
+    </div>
+  </div>;
+};
